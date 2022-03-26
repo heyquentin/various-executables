29027df2db355bd32ac9cbbc121c58f66c2f00bc
heyquentin <quentingauthier@gmail.com>
2022-03-25 18:55:38 -0700

    Update shellcode-exe.cpp

--- shellcode-exe.cpp
+++ shellcode-exe.cpp
@@ -1,3 +1,5 @@
+// Simple execution of shellcode, probably from msfvenom
+// Note to self, if you're using Visual Studio, change the architecture to x86 if you used 32bit msfvenom shellcode like windows/exec
 #include "Windows.h"
 
 int main()
